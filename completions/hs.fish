a01f8b88a0e7721cde107689274ecedc7254bab7
Dan Hoban <dan@doppio3.digital>
2020-04-06 13:09:40 +1000

    Base functions

---
+++ completions/hs.fish
@@ -0,0 +1,1 @@
+complete -c hs -w vagrant
---
+++ functions/hs.fish
@@ -0,0 +1,8 @@
+
+function hs -d "Global function for Laravel Homestead"
+    set DIR $PWD
+    set -q HOMESTEAD_PATH || set -Ux HOMESTEAD_PATH ~/Homestead
+    cd $HOMESTEAD_PATH
+    eval vagrant $argv
+    cd $DIR
+end
